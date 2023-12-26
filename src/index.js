dac5089a398625d4300031c98740cdab27f70116
Himanshu-Mendhe <himanshu.afcwp23@sinhgad.edu>
2023-12-26 16:03:22 +0530

    third commit to set up the project

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 const express = require ("express");
 
-require ('dotenv') . config();
+const {PORT} = require ('./config/serverConfig')
 
 const SetupAndStartServer = async () => {
     //create express object
     const app = express();
-    const PORT = 3000;
-
     app.listen (PORT , () =>  {
         console.log(`server started on port ${PORT}`);
-        console.log(process.env)
     })
 }
 SetupAndStartServer();
