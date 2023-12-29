802c3e9aba0db16fbf2dffc27e12fdf6db59c80d
Himanshu-Mendhe <himanshu.afcwp23@sinhgad.edu>
2023-12-29 18:34:27 +0530

    complited city_repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,26 +1,60 @@
 const { City } = require ('../models/index ');
 
 class cityRepository {
-    async createCity ({ name }){
+    
+    async createCity({ name }) {
         try{
-            const city = await City.create({ name });
-            return city;
-        } catch(error){
-            throw {error};  
+            const city = await City.create({
+                 name
+             })
+             return city;
+
+        }catch (error){
+            console.log("something went wrong in the city-repository folder");
+            throw {error};
         }
     }
 
     async deleteCity(cityId){
         try{
-            await City.destroy({
-                where : {
+            await City.destroy ({
+                where: {
                     id : cityId
                 }
-            });
-        }catch(error){
+            })
+            return true;
+        }catch (error){
+            console.log("something went wrong in the city-repository folder");
             throw {error};
         }
     }
+
+    async getCity(cityId) {
+        try{
+            const city = await City.findByPk(cityId);
+            return city;
+        }catch (error) {
+            console.log("something went wrong in the city-repository folder");
+            throw {error};
+        }
+
+    }
+
+    async updateCity(cityId,data) {
+        try{
+            const city = await City.update(data,{
+                where: {
+                    id : cityId
+                }
+            });
+            return city;
+        }catch (error){
+            console.log("something went wrong in the city-repository folder");
+            throw {error};
+        }
+
+    }
+
 }
 
 module.exports = cityRepository; 
